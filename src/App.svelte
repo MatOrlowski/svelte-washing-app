6ae46497c5f751f1872284f63c428c22854950f0
Mateusz Orłowski <mateusz0799@gmail.com>
2021-04-18 07:28:05 +0200

    Add sound when timer starts

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
   import HowTo from './HowTo.svelte';
   import Timer from './Timer.svelte';
+  let audio;
+  function timerStarts(){
+    audio.play();
+  }
 </script>
 
 <h1>HandwashingApp</h1>
-<Timer />
+<Timer on:start={timerStarts}/>
 <HowTo />
 
 <style>
@@ -12,3 +16,8 @@
     text-align: center;
   }
 </style>
+
+<audio bind:this={audio}>
+  <source src="Xylophone Tip Toe Scale Up.mp3"/>
+  <track kind="captions">
+</audio>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,10 +1,13 @@
 <script>
+  import {createEventDispatcher} from 'svelte';
   import ProgressBar from './ProgressBar.svelte';
   const totalSeconds = 20;
   let secondsLeft = totalSeconds;
   let isRunning = false;
   $: progress = ((totalSeconds - secondsLeft)/totalSeconds) * 100;
+  const dispatch = createEventDispatcher();
   function startTimer(){
+    dispatch('start');
     isRunning = true;
     let timer = setInterval(() => {
       secondsLeft -= 1;
