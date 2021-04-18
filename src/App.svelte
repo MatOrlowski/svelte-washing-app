2ca80ffbdaf607c3873fff563d73732c93ed69cb
Mateusz Orłowski <mateusz0799@gmail.com>
2021-04-18 07:36:40 +0200

    All done!

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,22 @@
   import HowTo from './HowTo.svelte';
   import Timer from './Timer.svelte';
   let audio;
-  function timerStarts(){
+  function timerStarts() {
     audio.play();
   }
 </script>
 
 <h1>HandwashingApp</h1>
-<Timer on:start={timerStarts}/>
+<Timer on:start={timerStarts} />
 <HowTo />
+
+<audio bind:this={audio}>
+  <source src="Xylophone Tip Toe Scale Up.mp3" />
+  <track kind="captions" />
+</audio>
 
 <style>
   h1 {
     text-align: center;
   }
 </style>
-
-<audio bind:this={audio}>
-  <source src="Xylophone Tip Toe Scale Up.mp3"/>
-  <track kind="captions">
-</audio>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,24 +1,23 @@
 <script>
-  import {createEventDispatcher} from 'svelte';
+  import { createEventDispatcher } from 'svelte';
   import ProgressBar from './ProgressBar.svelte';
   const totalSeconds = 20;
   let secondsLeft = totalSeconds;
   let isRunning = false;
-  $: progress = ((totalSeconds - secondsLeft)/totalSeconds) * 100;
+  $: progress = ((totalSeconds - secondsLeft) / totalSeconds) * 100;
   const dispatch = createEventDispatcher();
-  function startTimer(){
+  function startTimer() {
     dispatch('start');
     isRunning = true;
     let timer = setInterval(() => {
       secondsLeft -= 1;
-      if (secondsLeft == 0){
+      if (secondsLeft == 0) {
         clearInterval(timer);
         isRunning = false;
         secondsLeft = totalSeconds;
-      };
+      }
     }, 1000);
   }
-
 </script>
 
 <div bp="grid">
@@ -28,13 +27,14 @@
 <ProgressBar {progress} />
 
 <div bp="grid">
-<button 
-  disabled={isRunning}
-  bp="offset-5@md 4@md 12@sm" 
-  class="start"
-  on:click={startTimer}>
+  <button
+    disabled={isRunning}
+    bp="offset-5@md 4@md 12@sm"
+    class="start"
+    on:click={startTimer}
+  >
     Start
-</button>
+  </button>
 </div>
 
 <style>
@@ -46,7 +46,7 @@
     margin: 10px 0;
     border-radius: 10px;
   }
-  .start[disabled]{
+  .start[disabled] {
     background-color: gray;
     border-color: black;
     cursor: not-allowed;
